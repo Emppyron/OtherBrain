f73e283fcc6e2c92130affa2457cabb594f89d8d
high-ambitions <amankumar2021ap@gmail.com>
2025-04-10 02:46:33 +0530

    buttons frontend done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,19 @@
 
 import './App.css'
 import {Button} from "./components/button"
+import { PlusIcon } from "./components/Icons/plusIcon";
+import { ShareIcon } from './components/Icons/shareIcon';
 function App() {
   
 
   return (
     <>
-      hello js !
-      <Button variant="primary" size="md" text="asd" onClick={()=>{}}></Button>
-      <Button variant="secondary" size="md" text="asd" onClick={()=>{}}></Button>
+      <div className='flex gap-4 '>
+      <Button startIcon={<PlusIcon size="lg"></PlusIcon>} variant="primary" size="sm" text="Add" onClick={()=>{}}>
+      </Button>
+      <Button startIcon={<ShareIcon size="lg"></ShareIcon>} variant="secondary" size="sm" text="Share" onClick={()=>{}}>
+      </Button>
+      </div>
 
     </>
   )
--- frontend/src/components/button.tsx
+++ frontend/src/components/button.tsx
@@ -14,11 +14,22 @@ const Variants={
   "primary" : "bg-indigo-600 text-black",
   "secondary" : "bg-indigo-300 text-indigo-700"
 }
+const sizeStyles={
+  "sm" : "p-2",
+  "md" : "p-4",
+  "lg" : "p-6"
+}
+
+const defaultStyles="rounded-md flex";
 
 export const Button= (props : ButtonProps)=>{
   
                
-  return (<button className={Variants[props.variant]}>{props.variant}</button>);
+  return (  
+  <button className={`${Variants[props.variant]} ${defaultStyles} ${sizeStyles[props.size]}`}>
+    {props.startIcon? <div className="pr-2">{props.startIcon}</div> : null}
+    {props.text}
+    </button>);
 
 }
 
